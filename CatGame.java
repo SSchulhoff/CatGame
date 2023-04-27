0ca8a999f6d6dbc47b91d4c56496b3d02f2bc437
Schulhoff <seschulhoff@friendsbalt.org>
2023-04-27 11:53:53 -0400

    jonah sprayed water on my computer

--- CatGame.java
+++ CatGame.java
@@ -4,28 +4,31 @@ import java.util.Random;
 
 public class CatGame {
     private int n;
-    private int extraNode;
+    private int ghostNode;
+    private int catPos;
 
     private boolean[] marked;
 
     public CatGame(int n){
         this.n = n;
-        extraNode = n * n + 1;
-        EdgeWeightedGraph G = new EdgeWeightedGraph(extraNode);
+        ghostNode = n * n + 1;
+        EdgeWeightedGraph G = new EdgeWeightedGraph(ghostNode);
         marked = new boolean[n * n];
         for (int i = marked.length - 1; i > 0; i --){
             marked[i] = false;
         }
 
-        for (int row = 1; row < n - 1; row ++){
-            for (int col = 1; col < n - 1; col ++){
+        //need to fix it adds edges between vertices multiple times
+        for (int row = 0; row < n - 1; row ++){
+            for (int col = 0; col < n - 1; col ++){
                 int v = index(row, col);
-                G.addEdge(new CatEdge(v, v - 1));
                 G.addEdge(new CatEdge(v, v + 1));
-                G.addEdge(new CatEdge(v, v - n - 1));
-                G.addEdge(new CatEdge(v, v - n));
-                G.addEdge(new CatEdge(v, v + n));
-                G.addEdge(new CatEdge(v, v + n - 1));
+                if (row % 2 == 0){
+                    G.addEdge(new CatEdge(v, v + n));
+                }
+                else{
+                    G.addEdge(new CatEdge(v, v + n - 1));
+                }
             }
         }
 
@@ -44,17 +47,22 @@ public class CatGame {
     }
     
     public void markTile(int row, int col){
-        int n = index(row, col);
-        marked[n] = true;
+        marked[index(row, col)] = true;
+        //move the cat after this
     }
 
     public boolean marked(int row, int col){
-        return true;
+        if (marked[index(row, col)] == true)
+            return true;
+        return false;
     }
 
-    // public int[] getCatTile(){
-    //     return ;
-    // }
+    public int[] getCatTile(){
+        int[] tile = new int[2];
+        tile[0] = catPos / n;
+        tile[1] = catPos % n;
+        return tile;
+    }
 
     public boolean catHasEscaped(){
         return true;
@@ -70,4 +78,4 @@ public class CatGame {
     private int index(int row, int col){
         return n * row + col;
     }
-}
+} 
